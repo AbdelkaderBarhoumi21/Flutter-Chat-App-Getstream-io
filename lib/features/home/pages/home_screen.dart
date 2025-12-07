3ca7444405b3d28775da475d2ffad5c80780f48a
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2025-12-07 14:53:03 +0100

    features:Add home screen navigation menu

--- lib/features/home/pages/home_screen.dart
+++ lib/features/home/pages/home_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app_gestream_io/core/common/widgets/avatar/avatar.dart';
+import 'package:flutter_chat_app_gestream_io/core/utils/constants/app_constants.dart';
 import 'package:flutter_chat_app_gestream_io/features/calls/pages/calls_screen.dart';
 import 'package:flutter_chat_app_gestream_io/features/contacts/pages/contacts_screen.dart';
+import 'package:flutter_chat_app_gestream_io/features/home/widgets/custom_icon_background.dart';
 import 'package:flutter_chat_app_gestream_io/features/home/widgets/custom_navigation_bar_view.dart';
 import 'package:flutter_chat_app_gestream_io/features/messages/pages/messages_screen.dart';
 import 'package:flutter_chat_app_gestream_io/features/notificattions/pages/notificattions_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ValueNotifier<int> pageIndex = ValueNotifier(0);
+  final ValueNotifier<String> title = ValueNotifier('Messages');
 
   final List<Widget> _pages = const [
     MessagesScreen(),
@@ -14,22 +25,54 @@ class HomeScreen extends StatelessWidget {
     CallsScreen(),
     ContactsScreen(),
   ];
-  final pageTitles = const [
-    'Messages',
-    'Notifications',
-    'Calls',
-    'Contacts',
-  ];
+  final _pageTitles = const ['Messages', 'Notifications', 'Calls', 'Contacts'];
 
-  void _onNavigationItemSelected(int index){
-    
+  void _onNavigationItemSelected(int index) {
+    title.value = _pageTitles[index];
+    pageIndex.value = index;
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[0],
-      bottomNavigationBar: CustomBottomNavigationBarView(),
-    );
+  void dispose() {
+    pageIndex.dispose();
+    title.dispose();
+    super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: ValueListenableBuilder(
+          valueListenable: title,
+          builder: (context, value, child) => Text(value),
+        ),
+        leadingWidth: 54,
+        leading: Align(
+          alignment: Alignment.centerRight,
+          child: IconBackground(
+            icon: Icons.search,
+            onTap: () {
+              logger.i('TODO search');
+            },
+          ),
+        ),
+        // actions: [
+        //   Padding(
+        //     padding: const EdgeInsets.only(right: 24.0),
+        //     child: Hero(
+        //       tag: 'hero-profile-picture',
+        //       child: Avatar.small(url: context.currentUserImage, onTap: () {}),
+        //     ),
+        //   ),
+        // ],
+      ),
+
+      body: ValueListenableBuilder(
+        valueListenable: pageIndex,
+        builder: (context, value, child) => _pages[value],
+      ),
+      bottomNavigationBar: CustomBottomNavigationBarView(
+        onItemSelected: _onNavigationItemSelected,
+      ),
+    );
 }
